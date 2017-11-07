b57522f7bd081a39502ae2585ef01df5c7f2bfcb
lizhen <294474470@qq.com>
2017-11-07 10:36:54 +0800

    dev register agent to switch

--- main.go
+++ main.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"github.com/astaxie/beego/logs"
+
+	"github.com/astaxie/beego"
+	_ "uniswitch-agent/src/web/api/routers"
+	"uniswitch-agent/src/web/req"
+)
+
+var (
+	uniSwitchHost = beego.AppConfig.String("uniswitch.host")
+	registerUrl   = beego.AppConfig.String("registerUrl")
+)
+
+func main() {
+	//start process data
+	logInit()
+	logs.Info("register agent")
+	url := "" + uniSwitchHost + registerUrl
+	registerParam := "" //todo get agent param
+	req.RegisterAgentToSwitch(url, registerParam)
+
+	logs.Info("beego start run")
+	beego.Run()
+}
+
+func logInit() {
+	//日志默认不输出调用的文件名和文件行号,如果你期望输出调用的文件名和文件行号,可以如下设置
+	logs.SetLogFuncCall(true)
+	//如果你的应用自己封装了调用 log 包,那么需要设置 SetLogFuncCallDepth,默认是 2,
+	// 也就是直接调用的层级,如果你封装了多层,那么需要根据自己的需求进行调整.
+	// logs 里面修改的话,此处请勿重复设置!
+	logs.EnableFuncCallDepth(true)
+	logs.SetLogFuncCallDepth(3)
+	beego.BConfig.Log.AccessLogs = true
+
+	//如果不想在控制台输出log相关的，可以打开下面设置
+	//if u want not output to console, open following line!
+	//beego.BeeLogger.DelLogger("console")
+
+	// order 顺序必须按照
+	// 1. logs.SetLevel(level)
+	// 2. logs.SetLogger(logs.AdapterMultiFile, log_config_str)
+	logs.SetLevel(logs.LevelDebug)
+	//logs.SetLevel(logs.LevelInfo)
+}
--- src/web/req/requst.go
+++ src/web/req/requst.go
@@ -14,3 +14,16 @@ func ReqGet() {
 	}
 	fmt.Println(str)
 }
+
+func RegisterAgentToSwitch(url string, jsonData string) (str string) {
+	req := httplib.Post(url)
+	//req.Header("Content-Encoding", "utf-8")
+	req.Header("Content-Type", "application/json; charset=utf-8")
+	req.Body(jsonData)
+	str, err := req.String()
+	if err != nil {
+		return ""
+	}
+	logs.Info(str)
+	return str
+}
